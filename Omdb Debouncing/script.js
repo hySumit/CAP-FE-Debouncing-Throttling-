cfda2be953d2d6a1933cdcb70f659e3702674e28
Sumit Kumar <sumitbth67@gmail.com>
2024-01-04 17:25:47 +0530

    added

---
+++ Omdb Debouncing/script.js
@@ -0,0 +1,142 @@
+// const apiKey = "b428d3ba"
+
+// function MakeApiCall(){
+//     let search = document.getElementById("search").value
+
+//     fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${search}`)
+    
+//     .then(response => response.json())
+//     .then(resData => display(resData))
+//     .catch(error = alert(error))
+// }
+
+// let timerId
+// function debounce(callBackfn,delay){
+//     if(timerId){
+//         clearTimeout(timerId)
+//     }
+
+//     timerId = setTimeout(function(){
+//         callBackfn()
+//     },delay)
+// }
+
+
+// function display(movieData) {
+//     // let seachContainer = document.getElementById("searchContainer")
+//     let disContainer = document.getElementById("searchContainer");
+
+//     disContainer.innerHTML = "";
+
+//     if (movieData.Response === "False") {
+//         let noData = document.createElement('div');
+//         noData.textContent = "No Result Found!";
+//         disContainer.appendChild(noData);
+//     } else {
+//         movieData.Search.forEach((movie) => {
+//             const card = document.createElement("div");
+//             card.className = "card";
+
+
+//             let poster = document.createElement("img")
+//             poster.src = movie.Poster
+//             poster.className = "poster"
+
+            
+//             let title = document.createElement("p")
+//             title.textContent = "Title : " + movie.Title
+//             title.className ="title"
+
+            
+//             let release = document.createElement("p")
+//             release.textContent = "Year : " + movie.Year;
+//             release.className = "rel"
+
+            
+//             let id = document.createElement("p")
+//             id.textContent = "IMDB-ID : " + movie.imdbID
+
+//             id.className="id"
+
+            
+//             let genre = document.createElement("p")
+//             genre.textContent = "Type : " + movie.Type
+
+//             genre.className = "genre"
+
+//             card.append(poster, title, release, genre);
+
+//             disContainer.appendChild(card);
+
+//             // seachContainer.append(card)
+//         });
+//     }
+// }
+
+const apiKey = "b428d3ba";
+const searchContainer = document.getElementById("searchContainer");
+
+function MakeApiCall() {
+    let search = document.getElementById("search").value;
+
+    fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${search}`)
+        .then(response => response.json())
+        .then(resData => display(resData))
+        .catch(error => alert(error));
+}
+
+let timerId;
+
+function debounce(callBackfn, delay) {
+    if (timerId) {
+        clearTimeout(timerId);
+    }
+
+    timerId = setTimeout(function () {
+        callBackfn();
+    }, delay);
+}
+
+function display(movieData) {
+    searchContainer.innerHTML = "";
+
+    if (movieData.Response === "False") {
+        let noData = document.createElement('div');
+        noData.textContent = "No Result Found!";
+        searchContainer.appendChild(noData);
+    } else {
+        movieData.Search.forEach((movie) => {
+            const card = document.createElement("div");
+            card.className = "card";
+
+            let poster = document.createElement("img");
+            poster.src = movie.Poster;
+            poster.className = "poster";
+
+            let title = document.createElement("p");
+            title.textContent = "Title : " + movie.Title;
+            title.className = "title";
+
+            let release = document.createElement("p");
+            release.textContent = "Year : " + movie.Year;
+            release.className = "rel";
+
+            let id = document.createElement("p");
+            id.textContent = "IMDB-ID : " + movie.imdbID;
+            id.className = "id";
+
+            let genre = document.createElement("p");
+            genre.textContent = "Type : " + movie.Type;
+            genre.className = "genre";
+
+            card.append(poster, title, release, genre);
+            searchContainer.appendChild(card);
+        });
+    }
+
+    
+    searchContainer.style.display = movieData.Search ? "block" : "none";
+}
+
+
+searchContainer.style.display = "none";
